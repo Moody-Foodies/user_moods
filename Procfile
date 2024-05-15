1e84f707744bad5c4d5068c04eeb9d44212bf905
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-05-15 08:45:44 -0400

    heroku config

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn -c user_mood/gunicorn_config.py user_mood.app:app
--- user_mood/app.py
+++ user_mood/app.py
@@ -1,5 +1,6 @@
 import json
 import datetime
+import os
 from flask import Flask, jsonify, request
 from flask.views import MethodView
 from flasgger import Swagger
@@ -11,7 +12,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://myuser:mypassword@localhost/moods'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
